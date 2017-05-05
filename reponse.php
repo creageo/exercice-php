4048145fbb6d3287cc57ff0528a20c1b2fd22f30
creageo <mari.geof.1@hotmail.com>
2017-05-05 10:37:23 +0200

    cccc

---
+++ reponse.php
@@ -0,0 +1,20 @@
+<pre>
+<?php
+print_r($_POST);
+/*
+echo($_POST["prenom"]);
+echo "</br>";
+echo($_POST["nom"]);
+echo "</br>";
+echo($_POST["email"]);
+echo "</br>";
+echo($_POST["pays"]);
+echo "</br>";
+echo($_POST["genre"]);
+echo "</br>";
+echo($_POST["sujet"]);
+echo "</br>";
+echo($_POST["message"]);
+*/
+?>
+</pre>
